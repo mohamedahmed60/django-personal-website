71f06c721bcae505de45aad554b15b084029c383
mohamedahmed60 <mohamedaljabri6666@gmail.com>
2022-12-29 23:07:55 +0300

    design models blog & about

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='posts/')
+    publish_data = models.DateTimeField(default=timezone.now)
+    content = models.TextField(max_length=1000)
+
+    def __str__(self) -> str:
+        return self.title
+
--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    blog_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='about')
+    subtitle = models.TextField(max_length=500)
+    aboutme = models.TextField(max_length=1000)
+    tw_link = models.URLField()
+    tk_in_link = models.URLField()
+    github_link = models.URLField()
