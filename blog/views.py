371f74766e9b5b7a8ec8aa85fda4cdd32514afa4
mohamedahmed60 <mohamedaljabri6666@gmail.com>
2023-01-04 23:41:11 +0300

    add class PostList , DetailList

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView , DetailView
 # Create your views here.
 
 def all_posts(requent):
@@ -10,3 +11,13 @@ def all_posts(requent):
 def post_detail(requent,id):
     object = Post.objects.get(id=id)
     return render(requent,'blog/post_detail.html',{'post':object})
+
+
+
+class PostList(ListView):
+    model = Post
+    # template_name = ''
+    # context = {}
+
+class PostDetail(DetailView):
+    model = Post
