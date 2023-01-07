9754df7e0f9ae4ecc2f4a8a68de7e722ad8d86d6
mohamedahmed60 <mohamedaljabri6666@gmail.com>
2023-01-07 03:42:59 +0300

    edit edit edit

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from .views import all_posts , post_detail , PostList , PostDetail
 app_name = 'blog'
 
 urlpatterns = [
-    path('',all_posts),
-    path('<int:id>', post_detail,name='post_detail'),
+    path('', all_posts),
+    path('<int:id>', post_detail,name='postdetail'),
 
     path('cbv/', PostList.as_view()),
     path('cbv/<int:pk>', PostDetail.as_view()),  # <int:pk>  نستخدم ال  <int:id>  بدل مااسمه
